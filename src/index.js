b7ea145601db85bd4ff8fa05ab85725e27b2ebdf
benjamin tulun <benjamin.tulun@sinnerschrader.com>
2023-01-06 12:44:31 +0100

    chore: simple code change that should raise an alert in codeQL

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ try {
 } catch (error) {
   // nothing
 }
+
+// raise an alert in CodeQL
+// https://github.com/github/codeql/blob/main/javascript/ql/src/Statements/DanglingElse.qhelp
+if (1==2) {
+  console.log("hello world");
+  } else {
+    console.log("hello world");
+  }
