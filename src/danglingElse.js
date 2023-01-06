02bc29b4d2e9b9adb37c4ea1b2a57389d5a5facf
benjamin tulun <benjamin.tulun@sinnerschrader.com>
2023-01-06 15:25:06 +0100

    chore: tidyup JS

--- src/danglingElse.js
+++ src/danglingElse.js
@@ -1,5 +1,7 @@
-// raise an alert in CodeQL
+// test CodeQL
 // https://github.com/github/codeql/blob/main/javascript/ql/src/Statements/DanglingElse.qhelp
+// this should throw a warning
+// depending on the repository security settings a PR is going to blocked
 
 function f() {
 	if (1==2)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-try {
-  console.log("hello world");
-} catch (error) {
-  // nothing
-}
-
-// raise an alert in CodeQL
-// https://github.com/github/codeql/blob/main/javascript/ql/src/Statements/DanglingElse.qhelp
-if (1==2) {
-  console.log("hello world");
-  } else {
-    console.log("hello world");
-  }
+console.log("hello world");
