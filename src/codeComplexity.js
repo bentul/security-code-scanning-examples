b7b6620a315d653ace48ed8acf5668ec6f0fda33
benjamin tulun <benjamin.tulun@sinnerschrader.com>
2023-01-10 15:32:07 +0100

    chore: add case - code complexity too high

---
+++ src/codeComplexity.js
@@ -0,0 +1,18 @@
+// https://www.sonarsource.com/docs/CognitiveComplexity.pdf
+
+function a() {
+  if (1==2) { console.log(1); }
+  if (1==2) { console.log(1); }
+  if (1==2) { console.log(1); }
+  if (1==2) { console.log(1); }
+  if (1==2) { console.log(1); }
+  if (1==2) { console.log(1); }
+  if (1==2) { console.log(1); }
+  if (1==2) { console.log(1); }
+  if (1==2) { console.log(1); }
+  if (1==2) { console.log(1); }
+  if (1==2) { console.log(1); }
+}
+
+
+a();
